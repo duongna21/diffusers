b34cfc20b920a170e59fca8014a8756a6040c8ca
duongna21 <lablaugrana01@gmail.com>
2022-09-28 14:22:26 +0700

    draft

---
+++ examples/textual_inversion/test.py
@@ -0,0 +1,7 @@
+import os
+from transformers import CLIPFeatureExtractor, FlaxCLIPTextModel, CLIPTokenizer
+pretrained_model_name_or_path = "stable-diffusion-v1-4" #@param {type:"string"}
+
+text_encoder = FlaxCLIPTextModel.from_pretrained(
+    os.path.join(pretrained_model_name_or_path, "text_encoder"), use_auth_token=True, from_pt=True
+)
