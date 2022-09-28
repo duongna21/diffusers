e53a59673fa33110ef7fef28c9bb47c89ddd2cca
duongna21 <lablaugrana01@gmail.com>
2022-09-28 14:34:00 +0700

    draft

--- examples/textual_inversion/test.py
+++ examples/textual_inversion/test.py
@@ -1,8 +1,8 @@
 import os
-# from torchvision import transforms
+from torchvision import transforms
 from transformers import CLIPFeatureExtractor, FlaxCLIPTextModel, CLIPTokenizer
-pretrained_model_name_or_path = "stable-diffusion-v1-4" #@param {type:"string"}
-
-text_encoder = FlaxCLIPTextModel.from_pretrained(
-    os.path.join(pretrained_model_name_or_path, "text_encoder"), use_auth_token=True, from_pt=True
-)
+# pretrained_model_name_or_path = "stable-diffusion-v1-4" #@param {type:"string"}
+#
+# text_encoder = FlaxCLIPTextModel.from_pretrained(
+#     os.path.join(pretrained_model_name_or_path, "text_encoder"), use_auth_token=True, from_pt=True
+# )
