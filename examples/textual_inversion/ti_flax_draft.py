c9434b46633ccf678b84ed24d6293b0705abfd68
duongna21 <lablaugrana01@gmail.com>
2022-09-28 14:25:35 +0700

    draft

--- examples/textual_inversion/ti_flax_draft.py
+++ examples/textual_inversion/ti_flax_draft.py
@@ -3,36 +3,36 @@ import argparse
 import itertools
 import math
 import os
-# import random
-# from pathlib import Path
-# from typing import Optional
-#
-# import numpy as np
-# import torch
-# import torch.nn.functional as F
-# import torch.utils.checkpoint
-# from torch.utils.data import Dataset
-#
-# import PIL
-# from diffusers import FlaxAutoencoderKL, FlaxDDPMScheduler, FlaxPNDMScheduler, FlaxStableDiffusionPipeline, FlaxUNet2DConditionModel
-# from diffusers.optimization import get_scheduler
-# from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
-# from huggingface_hub import HfFolder, Repository, whoami
-# from PIL import Image
-# from torchvision import transforms
-# from tqdm.auto import tqdm
+import random
+from pathlib import Path
+from typing import Optional
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.utils.checkpoint
+from torch.utils.data import Dataset
+
+import PIL
+from diffusers import FlaxAutoencoderKL, FlaxDDPMScheduler, FlaxPNDMScheduler, FlaxStableDiffusionPipeline, FlaxUNet2DConditionModel
+from diffusers.optimization import get_scheduler
+from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
+from huggingface_hub import HfFolder, Repository, whoami
+from PIL import Image
+from torchvision import transforms
+from tqdm.auto import tqdm
 from transformers import CLIPFeatureExtractor, FlaxCLIPTextModel, CLIPTokenizer
 
-# def image_grid(imgs, rows, cols):
-#     assert len(imgs) == rows*cols
-#
-#     w, h = imgs[0].size
-#     grid = Image.new('RGB', size=(cols*w, rows*h))
-#     grid_w, grid_h = grid.size
-#
-#     for i, img in enumerate(imgs):
-#         grid.paste(img, box=(i%cols*w, i//cols*h))
-#     return grid
+def image_grid(imgs, rows, cols):
+    assert len(imgs) == rows*cols
+
+    w, h = imgs[0].size
+    grid = Image.new('RGB', size=(cols*w, rows*h))
+    grid_w, grid_h = grid.size
+
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i%cols*w, i//cols*h))
+    return grid
 
 pretrained_model_name_or_path = "stable-diffusion-v1-4" #@param {type:"string"}
 #
