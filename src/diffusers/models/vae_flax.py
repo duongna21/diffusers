f2b26f1cd0534d0696c0f73b1ded6ac70d478d5e
duongna21 <lablaugrana01@gmail.com>
2022-09-29 09:51:24 +0700

    draft

--- src/diffusers/models/vae_flax.py
+++ src/diffusers/models/vae_flax.py
@@ -770,7 +770,7 @@ class FlaxAutoencoderKL(nn.Module, FlaxModelMixin, ConfigMixin):
 
         return self.init(rngs, sample)["params"]
 
-    def encode(self, sample, deterministic: bool = True, return_dict: bool = True):
+    def __call__(self, sample, deterministic: bool = True, return_dict: bool = True):
         sample = jnp.transpose(sample, (0, 2, 3, 1))
 
         hidden_states = self.encoder(sample, deterministic=deterministic)
@@ -796,17 +796,17 @@ class FlaxAutoencoderKL(nn.Module, FlaxModelMixin, ConfigMixin):
 
         return FlaxDecoderOutput(sample=hidden_states)
 
-    def __call__(self, sample, sample_posterior=False, deterministic: bool = True, return_dict: bool = True):
-        posterior = self.encode(sample, deterministic=deterministic, return_dict=return_dict)
-        if sample_posterior:
-            rng = self.make_rng("gaussian")
-            hidden_states = posterior.latent_dist.sample(rng)
-        else:
-            hidden_states = posterior.latent_dist.mode()
-
-        sample = self.decode(hidden_states, return_dict=return_dict).sample
-
-        if not return_dict:
-            return (sample,)
-
-        return FlaxDecoderOutput(sample=sample)
+    # def __call__(self, sample, sample_posterior=False, deterministic: bool = True, return_dict: bool = True):
+    #     posterior = self.encode(sample, deterministic=deterministic, return_dict=return_dict)
+    #     if sample_posterior:
+    #         rng = self.make_rng("gaussian")
+    #         hidden_states = posterior.latent_dist.sample(rng)
+    #     else:
+    #         hidden_states = posterior.latent_dist.mode()
+    #
+    #     sample = self.decode(hidden_states, return_dict=return_dict).sample
+    #
+    #     if not return_dict:
+    #         return (sample,)
+    #
+    #     return FlaxDecoderOutput(sample=sample)
