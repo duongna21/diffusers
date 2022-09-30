b8a7883d46e2267b992e8a3dbdcb35cb116985c4
duongna21 <lablaugrana01@gmail.com>
2022-09-30 20:52:32 +0700

    draft

--- examples/textual_inversion/ti_flax_draft.py
+++ examples/textual_inversion/ti_flax_draft.py
@@ -491,6 +491,7 @@ for epoch in range(num_train_epochs):
     for step, batch in enumerate(train_dataloader):
         print('step: ', step)
         batch = tree_map(lambda x: x.numpy(), batch)
+        batch = shard(batch)
         state, train_metric, dropout_rngs = p_train_step(state, batch, dropout_rngs)
         train_metric = jax_utils.unreplicate(train_metric)
         # print(train_metrics)
