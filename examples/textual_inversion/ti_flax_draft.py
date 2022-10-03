e0c75a9453b8a2f29e22ab9488bc2bc54bb5bd19
duongna21 <lablaugrana01@gmail.com>
2022-10-03 18:05:01 +0700

    draft

--- examples/textual_inversion/ti_flax_draft.py
+++ examples/textual_inversion/ti_flax_draft.py
@@ -516,12 +516,12 @@ def train_step(state, batch, dropout_rng):
     print(token_embedding_grad.shape, placeholder_token_id)
     placeholder_token_grad = token_embedding_grad[placeholder_token_id]
     # print('placeholder_token_grad: ', placeholder_token_grad)
-    print('before zero grad: ', grad['text_model']['embeddings']['token_embedding']['embedding'][placeholder_token_id].mean())
-    print('before zero grad: ', grad['text_model']['embeddings']['token_embedding']['embedding'][batch["input_ids"][0][:10]].mean(-1))
+    print('before zero grad: ', jnp.abs(grad['text_model']['embeddings']['token_embedding']['embedding'][placeholder_token_id]).mean())
+    print('before zero grad: ', jnp.abs(grad['text_model']['embeddings']['token_embedding']['embedding'][batch["input_ids"][0][:10]]).mean(-1))
 
     grad['text_model']['embeddings']['token_embedding']['embedding'] = jnp.zeros_like(token_embedding_grad).at[placeholder_token_id].set(placeholder_token_grad)
-    print('\nafter set back last grad: ', grad['text_model']['embeddings']['token_embedding']['embedding'][placeholder_token_id].mean())
-    print('after set back last grad: ', grad['text_model']['embeddings']['token_embedding']['embedding'][batch["input_ids"][0][:10]].mean(-1))
+    print('\nafter set back last grad: ', jnp.abs(grad['text_model']['embeddings']['token_embedding']['embedding'][placeholder_token_id]).mean())
+    print('after set back last grad: ', jnp.abs(grad['text_model']['embeddings']['token_embedding']['embedding'][batch["input_ids"][0][:10]]).mean(-1))
     # print('batch["input_ids"][0]: ', batch["input_ids"][0])
     # print(token_embedding_grad[batch["input_ids"][0]][:10])
     # print('grad: ', tree_map(lambda x: x.shape, grad))
