ccbf35363cd1bd3ec8b3153006642dfb4eac4964
duongna21 <lablaugrana01@gmail.com>
2022-10-12 23:18:33 +0700

    draft

--- examples/textual_inversion/textual_inversion_flax.py
+++ examples/textual_inversion/textual_inversion_flax.py
@@ -591,7 +591,7 @@ def main():
 
         grad_fn = jax.value_and_grad(compute_loss)
         loss, grad = grad_fn(state.params)
-        # grad = jax.lax.pmean(grad, "batch")
+        grad = jax.lax.pmean(grad, "batch")
 
         token_embedding_grad = grad['text_model']['embeddings']['token_embedding']['embedding']
         placeholder_token_grad = token_embedding_grad[placeholder_token_id]
@@ -602,7 +602,7 @@ def main():
         new_state = state.apply_gradients(grads=grad)
 
         metrics = {"loss": loss}
-        # metrics = jax.lax.pmean(metrics, axis_name="batch")
+        metrics = jax.lax.pmean(metrics, axis_name="batch")
         compare_params(state.params, new_state.params, 0)
         return new_state, metrics, new_train_rng
 
@@ -610,7 +610,7 @@ def main():
     p_train_step = jax.pmap(train_step, "batch", donate_argnums=(0,))
 
     # Replicate the train state on each device
-    # state = jax_utils.replicate(state)
+    state = jax_utils.replicate(state)
 
     # Train!
     total_batch_size = args.train_batch_size * jax.local_device_count()
@@ -641,23 +641,20 @@ def main():
         train_step_progress_bar = tqdm(total=steps_per_epoch, desc="Training...", position=1, leave=False)
         # train
         for batch in train_dataloader:
-            break
-            # batch = shard(batch)
-            # state, train_metric, train_rngs = p_train_step(state, batch, train_rngs)
-            state, train_metric, rng = train_step(state, batch, rng)
+            batch = shard(batch)
+            state, train_metric, train_rngs = p_train_step(state, batch, train_rngs)
             train_metrics.append(train_metric)
 
             train_step_progress_bar.update(1)
-            break
 
-        # train_time += time.time() - train_start
-        #
-        # # train_metric = jax_utils.unreplicate(train_metric)
-        #
-        # train_step_progress_bar.close()
-        # epochs.write(
-        #     f"Epoch... ({epoch + 1}/{args.num_train_epochs} | Loss: {train_metric['loss']})"
-        # )
+        train_time += time.time() - train_start
+
+        train_metric = jax_utils.unreplicate(train_metric)
+
+        train_step_progress_bar.close()
+        epochs.write(
+            f"Epoch... ({epoch + 1}/{args.num_train_epochs} | Loss: {train_metric['loss']})"
+        )
 
         # Create the pipeline using using the trained modules and save it.
         if jax.process_index() == 0:
