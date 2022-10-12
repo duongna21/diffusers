0308475f87e319cbb54362b85bc31b04cc02c01c
duongna21 <lablaugrana01@gmail.com>
2022-10-12 10:00:24 +0700

    draft

--- examples/textual_inversion/textual_inversion_flax.py
+++ examples/textual_inversion/textual_inversion_flax.py
@@ -585,7 +585,7 @@ def main():
 
         grad_fn = jax.value_and_grad(compute_loss)
         loss, grad = grad_fn(state.params)
-        grad = jax.lax.pmean(grad, "batch")
+        # grad = jax.lax.pmean(grad, "batch")
 
         token_embedding_grad = grad['text_model']['embeddings']['token_embedding']['embedding']
         print("token_embedding_grad.shape, placeholder_token_id: ", token_embedding_grad.shape, placeholder_token_id)
@@ -612,7 +612,7 @@ def main():
         new_state = state.apply_gradients(grads=grad)
 
         metrics = {"loss": loss}
-        metrics = jax.lax.pmean(metrics, axis_name="batch")
+        # metrics = jax.lax.pmean(metrics, axis_name="batch")
 
         return new_state, metrics, new_dropout_rng
 
@@ -620,7 +620,7 @@ def main():
     p_train_step = jax.pmap(train_step, "batch", donate_argnums=(0,))
 
     # Replicate the train state on each device
-    state = jax_utils.replicate(state)
+    # state = jax_utils.replicate(state)
 
     # Train!
     total_batch_size = args.train_batch_size * jax.local_device_count()
@@ -646,8 +646,9 @@ def main():
         train_step_progress_bar = tqdm(total=steps_per_epoch, desc="Training...", position=1, leave=False)
         # train
         for batch in train_dataloader:
-            batch = shard(batch)
-            state, train_metric, dropout_rngs = p_train_step(state, batch, dropout_rngs)
+            # batch = shard(batch)
+            # state, train_metric, dropout_rngs = p_train_step(state, batch, dropout_rngs)
+            state, train_metric, rng = train_step(state, batch, rng)
             train_metrics.append(train_metric)
 
             train_step_progress_bar.update(1)
