80f3eff387f03132d407d7028f08c3338c6f6415
duongna21 <lablaugrana01@gmail.com>
2022-10-12 23:12:38 +0700

    draft

--- examples/textual_inversion/textual_inversion_flax.py
+++ examples/textual_inversion/textual_inversion_flax.py
@@ -641,6 +641,7 @@ def main():
         train_step_progress_bar = tqdm(total=steps_per_epoch, desc="Training...", position=1, leave=False)
         # train
         for batch in train_dataloader:
+            break
             # batch = shard(batch)
             # state, train_metric, train_rngs = p_train_step(state, batch, train_rngs)
             state, train_metric, rng = train_step(state, batch, rng)
@@ -649,14 +650,14 @@ def main():
             train_step_progress_bar.update(1)
             break
 
-        train_time += time.time() - train_start
-
-        # train_metric = jax_utils.unreplicate(train_metric)
-
-        train_step_progress_bar.close()
-        epochs.write(
-            f"Epoch... ({epoch + 1}/{args.num_train_epochs} | Loss: {train_metric['loss']})"
-        )
+        # train_time += time.time() - train_start
+        #
+        # # train_metric = jax_utils.unreplicate(train_metric)
+        #
+        # train_step_progress_bar.close()
+        # epochs.write(
+        #     f"Epoch... ({epoch + 1}/{args.num_train_epochs} | Loss: {train_metric['loss']})"
+        # )
 
         # Create the pipeline using using the trained modules and save it.
         if jax.process_index() == 0:
