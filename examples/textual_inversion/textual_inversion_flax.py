5026b6a88cef42553a33ab8daf00dcfa6dbeb5df
duongna21 <lablaugrana01@gmail.com>
2022-10-13 12:55:55 +0700

    draft

--- examples/textual_inversion/textual_inversion_flax.py
+++ examples/textual_inversion/textual_inversion_flax.py
@@ -607,7 +607,7 @@ def main():
         for batch in train_dataloader:
             # batch = shard(batch)
             # state, train_metric, train_rngs = p_train_step(state, batch, train_rngs)
-            state, train_metric, rng = p_train_step(state, batch, rng)
+            state, train_metric, rng = train_step(state, batch, rng)
             train_metrics.append(train_metric)
 
             train_step_progress_bar.update(1)
