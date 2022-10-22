c6af19e6f8b89f0fc2497adc349a0131ee23df6d
duongna21 <lablaugrana01@gmail.com>
2022-10-22 21:48:19 +0700

    draft

--- examples/text_to_image/train_text_to_image_flax.py
+++ examples/text_to_image/train_text_to_image_flax.py
@@ -388,7 +388,6 @@ def main():
         pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
         input_ids = [example["input_ids"] for example in examples]
         padded_tokens = tokenizer.pad({"input_ids": input_ids}, padding=True, return_tensors="pt")
-        print('padded_tokens.input_ids.shape: ', padded_tokens.input_ids.shape)
         batch = {
             "pixel_values": pixel_values,
             "input_ids": padded_tokens.input_ids,
