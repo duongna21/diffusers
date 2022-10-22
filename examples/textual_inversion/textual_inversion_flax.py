b1bd4412454892d77ac3f26396ae3cac82298289
duongna21 <lablaugrana01@gmail.com>
2022-10-22 21:17:44 +0700

    draft

--- examples/textual_inversion/textual_inversion_flax.py
+++ examples/textual_inversion/textual_inversion_flax.py
@@ -421,6 +421,7 @@ def main():
     def collate_fn(examples):
         pixel_values = torch.stack([example["pixel_values"] for example in examples])
         input_ids = torch.stack([example["input_ids"] for example in examples])
+        print('\n\ninput_ids.shape: ', input_ids.shape)
 
         batch = {"pixel_values": pixel_values, "input_ids": input_ids}
         batch = {k: v.numpy() for k, v in batch.items()}
