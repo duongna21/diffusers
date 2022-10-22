a1fc3f2ae09563930846265203c3ccea007f9c32
duongna21 <lablaugrana01@gmail.com>
2022-10-22 21:11:55 +0700

    draft

--- examples/text_to_image/train_text_to_image_flax.py
+++ examples/text_to_image/train_text_to_image_flax.py
@@ -386,8 +386,9 @@ def main():
     def collate_fn(examples):
         pixel_values = torch.stack([example["pixel_values"] for example in examples])
         pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
-        input_ids = torch.stack([example["input_ids"] for example in examples])
+        input_ids = [example["input_ids"] for example in examples]
         padded_tokens = tokenizer.pad({"input_ids": input_ids}, padding=True, return_tensors="pt")
+        print('padded_tokens.input_ids.shape: ', padded_tokens.input_ids.shape)
         batch = {
             "pixel_values": pixel_values,
             "input_ids": padded_tokens.input_ids,
