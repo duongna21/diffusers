2e1f99a10e3f999afd755374341f50bb29f53e1b
duongna21 <lablaugrana01@gmail.com>
2022-10-25 13:55:59 +0700

    draft

--- examples/textual_inversion/textual_inversion_flax.py
+++ examples/textual_inversion/textual_inversion_flax.py
@@ -484,8 +484,8 @@ def main():
     # state = train_state.TrainState.create(apply_fn=text_encoder.__call__, params=text_encoder.params, tx=optimizer)
     text_encoder_state = train_state.TrainState.create(apply_fn=text_encoder.__call__, params=params['text_encoder'],
                                                        tx=tx)
-    vae_state = train_state.TrainState.create(apply_fn=vae.encode, params=params['vae'], tx=optimizer)
-    unet_state = train_state.TrainState.create(apply_fn=unet.__call__, params=params['unet'], tx=optimizer)
+    # vae_state = train_state.TrainState.create(apply_fn=vae.encode, params=params['vae'], tx=optimizer)
+    # unet_state = train_state.TrainState.create(apply_fn=unet.__call__, params=params['unet'], tx=optimizer)
     noise_scheduler = FlaxDDPMScheduler(
         beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000
     )
@@ -494,13 +494,13 @@ def main():
     train_rngs = jax.random.split(rng, jax.local_device_count())
 
     # Define gradient train step fn
-    def train_step(text_encoder_state, vae_state, unet_state, batch, train_rng):
+    def train_step(text_encoder_state, batch, train_rng):
         params = {"text_encoder": text_encoder_state.params,
-                  "vae": vae_state.params,
-                  "unet": unet_state.params}
+                  "vae": state_vae,
+                  "unet": state_unet}
         dropout_rng, sample_rng, new_train_rng = jax.random.split(train_rng, 3)
 
-        def compute_loss(params, latents):
+        def compute_loss(params):
             # vae_outputs = vae_state.apply_fn(batch["pixel_values"], deterministic=False)
             vae_outputs = vae.apply(
                 {"params": params['vae']}, batch["pixel_values"], deterministic=True, method=vae.encode
@@ -557,8 +557,8 @@ def main():
 
     # Replicate the train state on each device
     text_encoder_state = jax_utils.replicate(text_encoder_state)
-    vae_state = jax_utils.replicate(vae_state)
-    unet_state = jax_utils.replicate(unet_state)
+    # vae_state = jax_utils.replicate(vae_state)
+    # unet_state = jax_utils.replicate(unet_state)
 
     # Train!
     num_update_steps_per_epoch = math.ceil(len(train_dataloader))
@@ -587,7 +587,7 @@ def main():
         # train
         for batch in train_dataloader:
             batch = shard(batch)
-            text_encoder_state, train_metric, train_rngs = p_train_step(text_encoder_state, vae_state, unet_state, batch, train_rngs)
+            text_encoder_state, train_metric, train_rngs = p_train_step(text_encoder_state, batch, train_rngs)
             train_metrics.append(train_metric)
 
             train_step_progress_bar.update(1)
