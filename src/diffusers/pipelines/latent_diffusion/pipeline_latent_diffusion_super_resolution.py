62552cebd33bd43b17d406276ee705cc67fa6f73
duongna21 <lablaugrana01@gmail.com>
2022-11-02 16:08:20 +0700

    ahi

--- src/diffusers/pipelines/latent_diffusion/pipeline_latent_diffusion_super_resolution.py
+++ src/diffusers/pipelines/latent_diffusion/pipeline_latent_diffusion_super_resolution.py
@@ -128,7 +128,8 @@ class LDMSuperResolutionPipeline(DiffusionPipeline):
         # decode the image latents with the VAE
         image = self.vqvae.decode(latents).sample
 
-        image = (image / 2 + 0.5).clamp(0, 1)
+        image = (image / 2 + 0.5).clamp(1, 1)
+        image = image * 255
         image = image.cpu().permute(0, 2, 3, 1).numpy()
         if output_type == "pil":
             image = self.numpy_to_pil(image)
