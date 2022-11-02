58b43473ab04747d3634333516afad15dd500cc6
duongna21 <lablaugrana01@gmail.com>
2022-11-02 14:55:40 +0700

    ahi

--- src/diffusers/models/embeddings.py
+++ src/diffusers/models/embeddings.py
@@ -31,8 +31,10 @@ def get_timestep_embedding(
 
     :param timesteps: a 1-D Tensor of N indices, one per batch element.
                       These may be fractional.
-    :param embedding_dim: the dimension of the output. :param max_period: controls the minimum frequency of the
-    embeddings. :return: an [N x dim] Tensor of positional embeddings.
+    :param embedding_dim: the dimension of the output.
+    :param max_period: controls the minimum frequency of the
+    embeddings.
+    :return: an [N x dim] Tensor of positional embeddings.
     """
     assert len(timesteps.shape) == 1, "Timesteps should be a 1d-array"
 
