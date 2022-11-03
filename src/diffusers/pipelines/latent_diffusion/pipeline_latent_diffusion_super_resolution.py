14e0a686d367b7a0767ee9f262b8740177589bad
duongna21 <lablaugrana01@gmail.com>
2022-11-03 15:09:09 +0700

    ahi

--- src/diffusers/pipelines/latent_diffusion/pipeline_latent_diffusion_super_resolution.py
+++ src/diffusers/pipelines/latent_diffusion/pipeline_latent_diffusion_super_resolution.py
@@ -105,7 +105,7 @@ class LDMSuperResolutionPipeline(DiffusionPipeline):
         )
         latents = latents.to(self.device)
         init_image = init_image.to(device=self.device, dtype=latents.dtype)
-        print(f'\nencode: {self.vqvae.encode(init_image).latent}')
+        print(f'\nencode: {self.vqvae.encode(init_image).latents}')
 
 
         # set timesteps
