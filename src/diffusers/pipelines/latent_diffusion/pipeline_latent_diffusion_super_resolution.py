31f660e12d63c4895d1bc3beab56337e07f633af
duongna21 <lablaugrana01@gmail.com>
2022-11-03 11:48:35 +0700

    ahi

--- src/diffusers/pipelines/latent_diffusion/pipeline_latent_diffusion_super_resolution.py
+++ src/diffusers/pipelines/latent_diffusion/pipeline_latent_diffusion_super_resolution.py
@@ -4,6 +4,7 @@ from typing import List, Optional, Tuple, Union
 import torch
 import torch.utils.checkpoint
 import PIL
+import torchvision
 
 from ...models import AutoencoderKL, UNet2DConditionModel, UNet2DModel, VQModel
 from ...pipeline_utils import DiffusionPipeline, ImagePipelineOutput
@@ -14,9 +15,8 @@ def preprocess(image):
     w, h = image.size
     w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
     image = image.resize((w, h), resample=PIL.Image.LANCZOS)
-    image = np.array(image).astype(np.float32) / 255.0
+    image = torch.unsqueeze(torchvision.transforms.ToTensor()(image), 0)
     image = image[None].transpose(0, 3, 1, 2)
-    image = torch.from_numpy(image)
     return 2.0 * image - 1.0
 
 class LDMSuperResolutionPipeline(DiffusionPipeline):
